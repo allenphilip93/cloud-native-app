274117c66ee0d4d36888810d46572a628bc486d8
Allen Philip <allenphilip93@gmail.com>
2021-04-20 13:21:20 +0530

    Fixed centralized API doc using open api

--- products-service/src/main/java/com/cloudnative/services/products/ProductsApiApplication.java
+++ products-service/src/main/java/com/cloudnative/services/products/ProductsApiApplication.java
@@ -1,13 +1,34 @@
 package com.cloudnative.services.products;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(info =
+	@Info(title = "Products API", version = "1.0", description = "Documentation Products API v1.0")
+)
 public class ProductsApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductsApiApplication.class, args);
     }
+
+    @Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new io.swagger.v3.oas.models.info.Info().title("Products API").version(appVersion)
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 
 }
